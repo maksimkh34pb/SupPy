ea1c03f7d275b3d2a2c197d01e91c18a76343df3
maksimkh34@dell <maksimkh34@duck.com>
2024-05-28 22:21:35 +0300

    bot start

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+import asyncio
+import os
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import telegram
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from dotenv import load_dotenv
 
 
-# Press the green button in the gutter to run the script.
+async def main():
+    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+
+    bot = telegram.Bot(os.environ.get('BOT_TOKEN'))
+    async with bot:
+        print(await bot.get_me())
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    asyncio.run(main())
